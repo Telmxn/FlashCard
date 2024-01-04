bf2a09a6d761542333d41c8b5bbe5188f05754de
Telman Yusifov <telmanyusifov78@gmail.com>
2024-01-04 13:12:49 +0400

    Test

--- src/api/index.js
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-export const instance = axios.create({
-  baseURL: "https://my-json-server.typicode.com/RockaHelller/FlashcardDatabase/",
-});
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,10 @@
-import { BrowserRouter } from "react-router-dom";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import { store } from "./store";
-import { Provider } from "react-redux";
-import React from 'react';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </BrowserRouter>
-  </Provider>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
